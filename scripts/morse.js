40c62b0614f6214f7601718ba700305f84138d2c
Janet Suryadi <janet@suryadi.id.au>
2023-06-26 19:53:34 +1000

    Adding constants and writing tests

---
+++ scripts/morse.js
@@ -0,0 +1,24 @@
+
+/*----------  Imports  ----------*/
+import * as c from './constants.js'
+
+
+/*----------  Function Declaration  ----------*/
+
+const translateEToMC = (inputString) => {
+    console.log(inputString);
+}
+
+const translateMCToE = (inputString) => {
+    console.log(inputString);
+}
+
+/*----------  Variable Declaration  ----------*/
+
+
+
+
+/*----------  Script assignment  ----------*/
+
+
+
--- scripts/morse.test.js
+++ scripts/morse.test.js
@@ -4,22 +4,33 @@ describe('Morse Code Translator Tests', () => {
 
     /*----------  Input Checking----------*/
     test('Should be able to take in an input of any length', () => {
-        expect(mc.translate("Hello world!")).toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--");
-        expect(mc.translate("A")).toBe(".-");
-        expect(mc.translate(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--")).toBe("Hello world!");
+        expect(mc.translateEToMC("Hello world!")).toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--");
+        expect(mc.translateEToMC("A")).toBe(".-");
+        expect(mc.translateMCtoE(".-")).toBe("A");
+        expect(mc.translateMCtoE(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--")).toBe("Hello world!");
     });
 
     test('Should throw an appropriate error if the input is not a string or is empty', () => {
         expect(() => {
-            mc.translate(42)
+            mc.translateEToMC(42)
+        }).toThrow(wrongTypeError);
+        expect(() => {
+            mc.translateMCtoE(42)
         }).toThrow(wrongTypeError);
 
         expect(() => {
-            mc.translate([1, f, y, 5])
+            mc.translateEToMC([1, f, y, 5])
         }).toThrow(wrongTypeError);
 
         expect(() => {
-            mc.translate('');
+            mc.translateMCtoE([1, f, y, 5])
+        }).toThrow(wrongTypeError);
+
+        expect(() => {
+            mc.translateEToMC('');
+        }).toThrow(emptyInputError);
+        expect(() => {
+            mc.translateMCtoE('');
         }).toThrow(emptyInputError);
     });
 })
