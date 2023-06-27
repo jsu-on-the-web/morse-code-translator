6685493c06e98941d936b83d5b9ba408b3350ecb
Janet Suryadi <janet@suryadi.id.au>
2023-06-27 20:15:11 +1000

    Starting to code the translations but tests aren't working

--- scripts/morse.js
+++ scripts/morse.js
@@ -5,15 +5,33 @@ import * as c from './constants.js'
 
 /*----------  Function Declaration  ----------*/
 
-const translateEToMC = (inputString) => {
+export const translateEToMC = (inputString) => {
+    // 1. Check if the input string is > 0 and it's a string
+    // 1.5 If not, throw an error
+    // 2. Do a for-each iteration on the input string to figure out which key the current character in inputString matches to
+    // 3. Push that key's value followed by a whitespace space (' ') into the output array.
+
+    // Input check
+    if (inputString.length <= 0) {
+        throw c.emptyStringError;
+    } else if (typeof inputString !== 'string') {
+        throw c.wrongTypeError
+    }
+
     console.log(inputString);
 }
 
-const translateMCToE = (inputString) => {
+export const translateMCToE = (inputString) => {
     console.log(inputString);
+    // Input check
+    if (inputString.length <= 0) {
+        throw c.emptyStringError;
+    } else if (typeof inputString !== 'string') {
+        throw c.wrongTypeError
+    }
 }
 
-const assembleString = (input, mode) => {
+export const assembleString = (input, mode) => {
     console.log(input);
 }
 
--- scripts/morse.test.js
+++ scripts/morse.test.js
@@ -1,45 +1,46 @@
-import * as mc from '../scripts/morse.js'
+import { translateEToMC, translateMCtoE, assembleString } from '../scripts/morse.js'
+import * as c from '../scripts/constants.js'
 
 describe('Morse Code Translator Tests', () => {
 
     /*----------  Input Checking----------*/
     test('Should be able to take in an input of any length and translate it', () => {
-        expect(mc.translateEToMC("Hello world!")).toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--");
-        expect(mc.translateEToMC("A")).toBe(".-");
-        expect(mc.translateMCtoE(".-")).toBe("A");
-        expect(mc.translateMCtoE(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--")).toBe("Hello world!");
+        expect(translateEToMC("Hello world!")).toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--");
+        expect(translateEToMC("A")).toBe(".-");
+        expect(translateMCtoE(".-")).toBe("A");
+        expect(translateMCtoE(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--")).toBe("Hello world!");
     });
 
     test('Should throw an appropriate error if the input is not a string or is empty', () => {
         expect(() => {
-            mc.translateEToMC(42)
-        }).toThrow(wrongTypeError);
+            translateEToMC(42);
+        }).toThrow(c.wrongTypeError);
         expect(() => {
-            mc.translateMCtoE(42)
-        }).toThrow(wrongTypeError);
+            translateMCtoE(42);
+        }).toThrow(c.wrongTypeError);
 
         expect(() => {
-            mc.translateEToMC([1, f, y, 5])
-        }).toThrow(wrongTypeError);
+            translateEToMC([1, f, y, 5]);
+        }).toThrow(c.wrongTypeError);
 
         expect(() => {
-            mc.translateMCtoE([1, f, y, 5])
-        }).toThrow(wrongTypeError);
+            translateMCtoE([1, f, y, 5]);
+        }).toThrow(c.wrongTypeError);
 
         expect(() => {
-            mc.translateEToMC('');
-        }).toThrow(emptyInputError);
+            translateEToMC('');
+        }).toThrow(c.emptyInputError);
         expect(() => {
-            mc.translateMCtoE('');
-        }).toThrow(emptyInputError);
+            translateMCtoE('');
+        }).toThrow(c.emptyInputError);
     });
 
 
     /*----------  Assembly Test  ----------*/
     test('Should format English correctly', () => {
-        expect(mc.assembleString(["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", "!"], 'english')).toEqual('Hello world!');
+        expect(assembleString(["H", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", "!"], 'english')).toEqual('Hello world!');
     });
     test('Should format Morse Code correctly', () => {
-        expect(mc.assembleString(["....", " ", ".", " ", ".-..", " ", ".-..", " ", "---", " ", "/", " ", ".--", " ", "---", " ", ".-.", " ", ".-..", " ", "-..", " ", "-.-.--"], 'morse')).toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--");
+        expect(assembleString(["....", " ", ".", " ", ".-..", " ", ".-..", " ", "---", " ", "/", " ", ".--", " ", "---", " ", ".-.", " ", ".-..", " ", "-..", " ", "-.-.--"], 'morse')).toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--");
     });
 })
