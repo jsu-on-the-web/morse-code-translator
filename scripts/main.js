bde3a3ca3752682e0882211cc64efb680564419d
Janet Suryadi <janet@suryadi.id.au>
2023-06-30 17:14:37 +1000

    Renaming

--- scripts/main.js
+++ scripts/main.js
@@ -14,10 +14,10 @@ const grabInput = (event) => {
 
     let inputText = input.value;
     
-    if (eToMCSelect.checked === true) { 
+    if (englishToMorseCodeSelect.checked === true) { 
         console.log(`${inputText} will be translated to Morse Code`);
         output.value = m.translateEToMC(inputText);
-    } else if (mcToESelect.checked === true) {
+    } else if (morseCodeToEnglishSelect.checked === true) {
         console.log(`${inputText} will be translated to English`);
         output.value = m.translateMCToE(inputText);
     }
@@ -27,8 +27,8 @@ const grabInput = (event) => {
 
 let submitButton = document.querySelector('.input-form__submit-button');
 let inputForm = document.querySelector('.input-form');
-let eToMCSelect = document.querySelector('#eToMc');
-let mcToESelect = document.querySelector('#mcToE');
+let englishToMorseCodeSelect = document.querySelector('#englishToMorseCode');
+let morseCodeToEnglishSelect = document.querySelector('#morseCodeToEnglish');
 let input = document.querySelector('#input-textarea');
 let output = document.querySelector('#output-textarea');
 
